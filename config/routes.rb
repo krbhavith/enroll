4eeb223f2ba069eb9c1f9463b068cb5e1f5342cd
vineel <sonnathivinil@gmail.com>
2019-03-28 10:53:07 -0400

    Review page post submit

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
       get 'application_checklist',on: :collection, action: 'application_checklist', as: 'application_checklist'
       get 'get_help_paying_coverage_response',on: :collection, action: 'get_help_paying_coverage_response', as: 'get_help_paying_coverage_response'
       get :review_and_submit, on: :member
+      get :review, on: :member
       get :eligibility_results, on: :member
       get :wait_for_eligibility_response, on: :member
       get :check_eligibility_results_received, on: :member
